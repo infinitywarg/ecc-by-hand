cfa6d30d98623b84beef8b21c2e0c931636cecb5
infinitywarg <ganesh.k.anantwar@gmail.com>
2023-01-03 01:24:23 +0530

    working field subtraction

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::fmt::{Display, Result};
 
 fn main() {
-    let f1 = Fp::new(8, 13);
-    let f2 = Fp::new(9, 13);
-    match f1.add(&f2) {
+    let f1 = Fp::new(9, 13);
+    let f2 = Fp::new(3, 13);
+    match f1.sub(&f2) {
         None => {
             println!("error: different modulii cannot be added");
         }
         Some(f) => {
-            println!("{} + {} = {}", f1, f2, f);
+            println!("{} - {} = {}", f1, f2, f);
         }
     }
 }
@@ -23,6 +23,9 @@ trait Field {
     fn new(v: u32, m: u32) -> Self;
     fn check_modulii(m1: &u32, m2: &u32) -> bool;
     fn add(&self, other: &Self) -> Option<Self>
+    where
+        Self: Sized;
+    fn sub(&self, other: &Self) -> Option<Self>
     where
         Self: Sized;
     // fn mul(&self, other: &Self) -> Self;
@@ -54,9 +57,27 @@ impl Field for Fp {
             false
         }
     }
+
     fn add(&self, other: &Self) -> Option<Self> {
         if Self::check_modulii(&self.modulus, &other.modulus) {
             let value = (self.value + other.value) % self.modulus;
+            Some(Fp {
+                value: value,
+                modulus: self.modulus,
+            })
+        } else {
+            None
+        }
+    }
+
+    fn sub(&self, other: &Self) -> Option<Self> {
+        if Self::check_modulii(&self.modulus, &other.modulus) {
+            let value = if self.value >= other.value {
+                (self.value - other.value) % self.modulus
+            } else {
+                (other.value - self.value) % self.modulus
+            };
+
             Some(Fp {
                 value: value,
                 modulus: self.modulus,
