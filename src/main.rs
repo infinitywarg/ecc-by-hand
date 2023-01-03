8aac9a9a16785935bb3f67c51b188a5d0002530a
infinitywarg <ganesh.k.anantwar@gmail.com>
2023-01-03 21:25:12 +0530

    replace i32 to u32 as field elements are >=0

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fmt::{Display, Result};
 
 fn main() {
-    let f1 = Fp::new(23, 524269).unwrap();
-    let f2 = Fp::new(123, 524269).unwrap();
+    let f1 = Fp::new(10, 41).unwrap();
+    let f2 = Fp::new(11, 41).unwrap();
 
     match f1.add(&f2) {
         None => {
@@ -32,16 +32,29 @@ fn main() {
     }
 
     println!("inverse of {} = {}", f1, f1.inv().unwrap());
+    println!("square of {} = {}", f1, f1.sq().unwrap());
 }
 
 #[derive(Debug)]
-pub struct Fp {
-    value: i32,
-    modulus: i32,
+struct Fp {
+    value: u32,
+    modulus: u32,
 }
 
+struct Point {
+    x: u32,
+    y: u32,
+}
+
+struct Curve {
+    a: u32,
+    b: u32,
+}
+
+trait EccMath {}
+
 trait Field {
-    fn new(v: i32, m: i32) -> Option<Self>
+    fn new(v: u32, m: u32) -> Option<Self>
     where
         Self: Sized;
     fn check_input(&self, other: &Self) -> bool;
@@ -57,7 +70,13 @@ trait Field {
     fn inv(&self) -> Option<Self>
     where
         Self: Sized;
+    fn sq(&self) -> Option<Self>
+    where
+        Self: Sized;
     fn extended_euclidean(a: i32, b: i32) -> (i32, i32, i32);
+    // fn pow(self: &Self, other: &Self) -> Option<Self>
+    // where
+    //     Self: Sized;
 }
 
 impl Display for Fp {
@@ -67,13 +86,13 @@ impl Display for Fp {
 }
 
 impl Field for Fp {
-    fn new(v: i32, m: i32) -> Option<Self> {
+    fn new(v: u32, m: u32) -> Option<Self> {
         // check if m is prime
         if m <= 1 {
             return None;
         }
 
-        let mut i: i32 = 2;
+        let mut i: u32 = 2;
         while i * i <= m {
             if m % i == 0 {
                 return None;
@@ -109,11 +128,12 @@ impl Field for Fp {
 
     fn sub(self: &Self, other: &Self) -> Option<Self> {
         if Self::check_input(&self, &other) {
-            let mut value = self.value - other.value;
-            if value < 0 {
-                value += self.modulus;
+            let value;
+            if self.value < other.value {
+                value = other.value - self.value;
+            } else {
+                value = self.value - other.value;
             }
-
             Some(Fp {
                 value: value,
                 modulus: self.modulus,
@@ -136,12 +156,13 @@ impl Field for Fp {
     }
 
     fn inv(self: &Self) -> Option<Self> {
-        let (_gcd, mut u, _v) = Self::extended_euclidean(self.value, self.modulus);
+        let (_gcd, mut u, _v) = Self::extended_euclidean(self.value as i32, self.modulus as i32);
+        let modulus: i32 = (self.modulus as u32).try_into().unwrap();
         if u < 0 {
-            u += self.modulus;
+            u = u + modulus;
         }
         Some(Fp {
-            value: u,
+            value: u as u32,
             modulus: self.modulus,
         })
     }
@@ -153,4 +174,24 @@ impl Field for Fp {
         let (gcd, x, y) = Self::extended_euclidean(b, a % b);
         return (gcd, y, x - (a / b) * y);
     }
+
+    fn sq(&self) -> Option<Self> {
+        Some(Self::mul(&self, &self).unwrap())
+    }
+
+    // fn pow(self: &Self, other: &Self) -> Option<Self> {
+    //     if Self::check_input(&self, &other) {
+    //         let value = (u32::pow(self.value, other.value as u32)) % self.modulus;
+    //         Some(Fp {
+    //             value: value,
+    //             modulus: self.modulus,
+    //         })
+    //     } else {
+    //         None
+    //     }
+    // }
+
+    // fn sqrt(self: &Self) -> Option<Self> {
+
+    // }
 }
